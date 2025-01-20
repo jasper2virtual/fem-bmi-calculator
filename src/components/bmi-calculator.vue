7fceb4d49149579ecf1e3958a2bde047f1839f3e
Virtual Jasper <jasper2virtual@hotmail.com>
2025-01-20 18:11:27 +0800

    add units-converter dependency and update BMI calculator to support imperial and metric units

--- src/components/bmi-calculator.vue
+++ src/components/bmi-calculator.vue
@@ -7,23 +7,44 @@
         </div>
         <div class="flex flex-col">
             <label class="flex flex-col"><span>Height:</span>
-                <div><input type="number" v-model="heightInMetric.cm" /><div></div></div>
+                <div>
+                    <input type="number" v-model="heightInCm" />
+                </div>
+                <div>
+                    <input type="number" v-model="heightInFtPart" />
+                    <input type="number" v-model="heightInInPart" min="0" max="11" />
+                </div>
             </label>
             <label class="flex flex-col"><span>Weight:</span>
-                <div><input type="number" v-model="weightInMetric.kg" /><div></div></div>
+                <div><input type="number" v-model="weightInKg" />
+                    <div></div>
+                </div>
             </label>
         </div>
     </div>
 
 </template>
 <script lang="ts" setup>
+import { computed, ref } from 'vue'
 import { useBMI } from '/src/libs/bmi'
 const { weightInKg,
-        heightInM,
-        weightInLb,
-        heightInFt,
-        bmi,
-        idealWeightInKg,
-        idealWeightInLb } = useBMI()
-
+    heightInCm,
+    weightInLb,
+    heightInIn,
+    bmi,
+    idealWeightInKg,
+    idealWeightInLb } = useBMI()
+const system = ref<'imperial' | 'metric'>('metric')
+const heightInInPart = computed({
+    get: () => heightInIn.value % 12,
+    set: (val: number) => {
+        heightInIn.value = val + heightInFtPart.value * 12
+    }
+})
+const heightInFtPart = computed({
+    get: () => Math.floor(heightInIn.value / 12),
+    set: (val: number) => {
+        heightInIn.value = val * 12 + heightInInPart.value
+    }
+})
 </script>
--- src/libs/bmi.ts
+++ src/libs/bmi.ts
@@ -1,5 +1,6 @@
 import { computed, ref } from 'vue'
 import calcBMI from 'bmi-calc'
+import { length, mass } from 'units-converter'
 
 
 
@@ -12,43 +13,43 @@ export function useBMI() {
             _weightInKg.value = value
         }
     })
-    const heightInM = computed({
-        get: () => _heightInM.value,
+    const heightInCm = computed({
+        get: () => _heightInCm.value,
         set: (value) => {
-            _heightInM.value = value
+            _heightInCm.value = value
         }
     })
    
     const weightInLb = computed({
-        get: () => _weightInKg.value * 2.20462,
+        get: () => mass(_weightInKg.value).from('kg').to('lb').value,
         set: (value) => {
-            _weightInKg.value = value / 2.20462
+            _weightInKg.value = mass(value).from('lb').to('kg').value
         }
     })
-    const heightInFt = computed({
-        get: () => _heightInM.value * 3.28084,
+    const heightInIn = computed({
+        get: () => length(_heightInCm.value).from('cm').to('in').value,
         set: (value) => {
-            _heightInM.value = value / 3.28084
+            _heightInCm.value = length(value).from('in').to('cm').value
         }
     })
 
     const _weightInKg=ref<number>(0)
-    const _heightInM=ref<number>(0)
+    const _heightInCm = ref<number>(0)
 
     const bmi = computed(() => {
-        const { value: bmiValue, name: bmiCategory } = calcBMI(_weightInKg.value, _heightInM.value)
+        const { value: bmiValue, name: bmiCategory } = calcBMI(_weightInKg.value, length(_heightInCm.value).from('cm').to('m').value)
         return { bmiValue, bmiCategory }
     })
 
-    const idealWeightInKg = computed(() => 2.2 * bmi.value.bmiValue + (3.5 * bmi.value.bmiValue) * (_heightInM.value - 1.5))
-    const idealWeightInLb = computed(() => Math.floor(idealWeightInKg.value % 6.35029318))
+    const idealWeightInKg = computed(() => 20 * _heightInCm.value * _heightInCm.value)
+    const idealWeightInLb = computed(() => mass(idealWeightInKg.value).from('kg').to('lb').value)
 
 
     return {
         weightInKg,
-        heightInM,
+        heightInCm,
         weightInLb,
-        heightInFt,
+        heightInIn,
         bmi,
         idealWeightInKg,
         idealWeightInLb
