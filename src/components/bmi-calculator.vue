311ceef9cf01fbb259e8ddc199448ad1e3706785
Virtual Jasper <jasper2virtual@hotmail.com>
2025-01-22 23:04:04 +0800

    refactor BMI calculator layout; improve structure and readability of BMI result display

--- src/components/bmi-calculator.vue
+++ src/components/bmi-calculator.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="bg-white rounded-3xl flex flex-col gap-4 p-4">
-        <div class="app-text-heading-m" >Enter your details below</div>
+        <div class="app-text-heading-m">Enter your details below</div>
         <div class="flex app-text-body-m">
             <label class=" flex-1 flex gap-2"><input type="radio" value="metric" v-model="system" />Metric</label>
             <label class="flex-1 flex gap-2"><input type="radio" value="imperial" v-model="system" />Imperial</label>
         </div>
         <div class="flex flex-col gap-4" :class="{
-             'app-tablet:flex-row': system === 'metric',
+            'app-tablet:flex-row': system === 'metric',
         }">
             <label class="flex flex-col gap-2 app-tablet:flex-1">
                 <span>Height:</span>
@@ -37,24 +37,31 @@
                 </div>
             </label>
         </div>
-        <div class=" bg-app-blue text-white rounded-lg p-4 app-text-body-m flex flex-col gap-4 app-tablet:rounded-r-full app-tablet:flex-row app-tablet:justify-between app-tablet:items-center " v-if="bmi.bmiValue">
-            <div class="flex flex-col gap-2">
-                <div>Your BMI is...</div>
-                <div class="app-text-heading-l">{{ bmi.bmiValue.toFixed(1) }}</div>
+        <div class=" bg-app-blue text-white rounded-lg app-tablet:rounded-r-full p-4 app-text-body-m ">
+            <div v-if="bmi.bmiValue"
+                class="flex flex-col gap-4  app-tablet:flex-row app-tablet:justify-between app-tablet:items-center ">
+                <div class="flex flex-col gap-2">
+                    <div>Your BMI is...</div>
+                    <div class="app-text-heading-l">{{ bmi.bmiValue.toFixed(1) }}</div>
+                </div>
+                <div>
+                    Your BMI suggests you’re a {{ bmi.bmiCategory }}. Your ideal weight is between
+                    <span class="font-bold">
+                        <template v-if="system === 'metric'">
+                            {{ idealWeightInKg.min.toFixed(1) }}kgs - {{ idealWeightInKg.max.toFixed(1) }}kgs
+                        </template>
+                        <template v-else>
+                            {{ minIdealWeightIn_StPart.toFixed(0) }}st {{ minIdealWeightIn_LbPart.toFixed(0) }}lbs - {{
+                                maxIdealWeightIn_StPart.toFixed(0) }}st {{
+                                maxIdealWeightIn_LbPart.toFixed(0) }}lbs
+                        </template>
+                    </span>
+                    .
+                </div>
             </div>
-            <div>
-                Your BMI suggests you’re a {{ bmi.bmiCategory }}. Your ideal weight is between
-                <span class="font-bold">
-                    <template v-if="system === 'metric'">
-                        {{ idealWeightInKg.min.toFixed(1) }}kgs - {{ idealWeightInKg.max.toFixed(1) }}kgs
-                    </template>
-                    <template v-else>
-                        {{ minIdealWeightIn_StPart.toFixed(0) }}st {{ minIdealWeightIn_LbPart.toFixed(0) }}lbs - {{
-                            maxIdealWeightIn_StPart.toFixed(0) }}st {{
-                            maxIdealWeightIn_LbPart.toFixed(0) }}lbs
-                    </template>
-                </span>
-                .
+            <div v-else class="flex flex-col gap-4">
+                <span class="app-text-heading-m">Welcome!</span>
+                <span>Enter your height and weight and you’ll see your BMI result here</span>
             </div>
         </div>
     </div>
